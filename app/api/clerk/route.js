ef1870d44cbcc22bd9f109044f45deca7817e993
LalitEzbiz <lalit.sharma@ezbizsoft.in>
2025-04-15 16:45:41 +0530

    problem solved

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -13,7 +13,9 @@ export async function POST(req) {
     const headerPayload = headers();
 
     const svixHeaders = {
+
       "svix-id": headerPayload.get("svix-id"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
       "svix-signature": headerPayload.get("svix-signature"),
     };
 
